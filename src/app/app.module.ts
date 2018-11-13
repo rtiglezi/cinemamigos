f00fd35b9973e37b08690c116a924ce1047599f7
Ronaldo Tonioli Iglezias <ronaldo.iglezias@gmail.com>
2018-11-13 15:02:40 -0200

    funcionalidade de assistiu ou nao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule } from "angularfire2";
 
-import { AppComponent } from './app.component';
-import { AuthService } from './providers/auth.service';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { NavComponent } from './nav/nav.component';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
-import { MoovieDetailsComponent } from './moovie-details/moovie-details.component';
+import { AppComponent } from "./app.component";
+import { AuthService } from "./providers/auth.service";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { NavComponent } from "./nav/nav.component";
+import { ProfilePageComponent } from "./profile-page/profile-page.component";
+import { MoovieDetailsComponent } from "./moovie-details/moovie-details.component";
 
 export const firebaseConfig = {
-    apiKey: 'AIzaSyDamhy3gQqiMz0i_3nwoVJAyp4qU85AutM',
-    authDomain: 'cinemamigos-d0898.firebaseapp.com',
-    databaseURL: 'https://cinemamigos-d0898.firebaseio.com',
-    projectId: 'cinemamigos-d0898',
-    storageBucket: 'cinemamigos-d0898.appspot.com',
-    messagingSenderId: '1021556023678'
-  };
+  apiKey: "AIzaSyDamhy3gQqiMz0i_3nwoVJAyp4qU85AutM",
+  authDomain: "cinemamigos-d0898.firebaseapp.com",
+  databaseURL: "https://cinemamigos-d0898.firebaseio.com",
+  projectId: "cinemamigos-d0898",
+  storageBucket: "cinemamigos-d0898.appspot.com",
+  messagingSenderId: "1021556023678"
+};
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'profile', component: ProfilePageComponent },
-  { path: 'moovie', component: MoovieDetailsComponent }
+  { path: "", component: HomePageComponent },
+  { path: "login", component: LoginPageComponent },
+  { path: "profile", component: ProfilePageComponent },
+  { path: "moovie", component: MoovieDetailsComponent }
 ];
 
 @NgModule({
@@ -49,4 +49,5 @@ const routes: Routes = [
   providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/moovie-details/moovie-details.component.ts
+++ src/app/moovie-details/moovie-details.component.ts
@@ -1,10 +1,8 @@
-
-import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase } from "angularfire2/database";
+import { Component, OnInit } from "@angular/core";
 import { MooviesService } from "app/providers/moovies.service";
-
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "./../providers/auth.service";
 
 @Component({
   selector: "app-moovie-details",
@@ -14,6 +12,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class MoovieDetailsComponent implements OnInit {
 
+  localUser = this.authService.getLocalUser();
+
+  public classSim = "";
+  public classNao = "";
+
+  public assistido: boolean = false;
+
   public filme = new Object();
   public filmeid;
 
@@ -22,30 +27,69 @@ export class MoovieDetailsComponent implements OnInit {
   private sub: any;
 
   constructor(
+    public authService: AuthService,
     private route: ActivatedRoute,
     public moviesService: MooviesService,
-    private router: Router
-  ) {
-
-    }
+    private router: Router,
+    private db: AngularFireDatabase
+  ) {}
 
   ngOnInit() {
-      this.sub = this.route.queryParams.subscribe(qp => {
+    this.sub = this.route.queryParams.subscribe(qp => {
       this.id = qp["id"];
       this.origem = qp["origem"];
     });
 
     this.filmeid = this.id;
+
     this.moviesService.getMovieDetails(this.filmeid).subscribe(data => {
       const response = data as any;
       const objeto_retorno = JSON.parse(response._body);
       this.filme = objeto_retorno;
-      // console.log(this.filme);
     });
+
+    this.getMoovieService();
+
   }
 
   backClicked() {
-    this.router.navigate([''], { queryParams: { "origem": this.origem } });
+    this.router.navigate([""], { queryParams: { origem: this.origem } });
+  }
+
+  getMoovieService() {
+    this.db.list("watchedMoovies").subscribe(r => {
+      r.map(m=>{
+        if (m.$key === this.filmeid + "_" + this.localUser.user_uid){
+          if (m.assistido == true) {
+            this.assistido = true;
+          }
+          if (this.assistido) {
+            this.classSim = "btn btn-sm btn-danger";
+            this.classNao = "btn btn-sm btn-default";
+          } else {
+            this.classNao = "btn btn-sm btn-danger";
+            this.classSim = "btn btn-sm btn-default";
+          }
+        }
+      });
+    });
+  }
+
+  updateMoovieService(toogle) {
+    this.db.object("watchedMoovies/" + this.filmeid + "_" + this.localUser.user_uid).set({
+      filmeId: this.filmeid,
+      usuarioId: this.localUser.user_uid,
+      assistido: toogle
+    }).then(r => {
+      this.assistido = toogle;
+      if (this.assistido) {
+        this.classSim = "btn btn-sm btn-danger";
+        this.classNao = "btn btn-sm btn-default";
+      } else {
+        this.classNao = "btn btn-sm btn-danger";
+        this.classSim = "btn btn-sm btn-default";
+      }
+    });
   }
 
 }
