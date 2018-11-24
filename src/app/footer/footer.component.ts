143052fecc103e9249619863bee5cd004c819b7e
Ronaldo Tonioli Iglezias <ronaldo.iglezias@gmail.com>
2018-11-24 01:42:17 -0200

    melhorias no campo de busca

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,43 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { Router } from "@angular/router";
 import { MooviesService } from "app/providers/moovies.service";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
   providers: [MooviesService]
 })
 export class FooterComponent implements OnInit {
-
   arrayMoovies = [];
 
-  constructor(
-    private router: Router,
-    public moviesService: MooviesService
-    ) {
+  @ViewChild("srch") domSearch: ElementRef;
+  @ViewChild("srchIcon") domSearchIcon: ElementRef;
 
-    }
+  constructor(private router: Router, public moviesService: MooviesService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   navigate(destino) {
     this.router.navigate([destino]);
   }
 
   searchMoovies(query) {
+
+    this.domSearchIcon.nativeElement.innerHTML =
+    "<img src='../assets/loader-grey.gif' style='width:25px;'/>";
+    this.domSearchIcon.nativeElement.className = "form-control-feedback";
+
+
     this.moviesService.getMovies(query).subscribe(data => {
       const response = data as any;
       const objeto_retorno = JSON.parse(response._body);
-      console.log(objeto_retorno.results);
       this.arrayMoovies = objeto_retorno.results;
+
+      this.domSearchIcon.nativeElement.innerHTML =
+      "<i id='i_search' class='glyphicon glyphicon-search form-control-feedback'></i>";
+
     });
+
+    if (this.domSearch.nativeElement.value=="") {
+      this.domSearchIcon.nativeElement.innerHTML =
+      "<i id='i_search' class='glyphicon glyphicon-search form-control-feedback'></i>";
+    }
+
   }
 
   selectMoovie(id) {
     this.router.navigate(["moovie"], { queryParams: { id: id } });
   }
 
-
+  setFocus() {
+    setTimeout(() => {
+      this.domSearch.nativeElement.focus();
+    }, 1000);
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,5 @@
 import { AuthService } from "./../providers/auth.service";
-import { Component } from "@angular/core";
+import { Component, ViewChild, ElementRef } from "@angular/core";
 import { Router } from "@angular/router";
 import { MooviesService } from "app/providers/moovies.service";
 
@@ -12,6 +12,12 @@ import { MooviesService } from "app/providers/moovies.service";
 export class NavComponent {
 
   public usuario;
+
+  public arrayMoovies = [];
+
+  @ViewChild('srchNav') domSearchNav: ElementRef;
+  @ViewChild('srchIconNav') domSearchIconNav: ElementRef;
+
 
   constructor(
     public authService: AuthService,
@@ -29,4 +35,45 @@ export class NavComponent {
   go(pg) {
     this.router.navigate([pg]);
   }
+
+  setFocus() {
+    setTimeout(() => {
+      this.domSearchNav.nativeElement.focus();
+    },1000)
+  }
+
+  searchMoovies(query) {
+
+    this.domSearchIconNav.nativeElement.innerHTML =
+    "<img src='../assets/loader-grey.gif' style='width:25px;'/>";
+    this.domSearchIconNav.nativeElement.className = "form-control-feedback";
+
+
+    this.moviesService.getMovies(query).subscribe(data => {
+      const response = data as any;
+      const objeto_retorno = JSON.parse(response._body);
+      this.arrayMoovies = objeto_retorno.results;
+
+      if (this.arrayMoovies.length > 10) {
+        this.arrayMoovies.splice(11, this.arrayMoovies.length);
+      }
+      console.log(this.arrayMoovies.length);
+
+      this.domSearchIconNav.nativeElement.innerHTML =
+      "<i id='i_search' class='glyphicon glyphicon-search form-control-feedback'></i>";
+
+    });
+
+    if (this.domSearchNav.nativeElement.value=="") {
+      this.domSearchIconNav.nativeElement.innerHTML =
+      "<i id='i_search' class='glyphicon glyphicon-search form-control-feedback'></i>";
+    }
+
+  }
+
+  selectMoovie(id) {
+    this.router.navigate(["moovie"], { queryParams: { id: id } });
+  }
+
+
 }
